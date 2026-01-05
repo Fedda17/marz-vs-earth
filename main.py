023b592f74eeb04574fa6d4e3a594866fc6112ba
Fedda17 <fedda.martini@gmail.com>
2026-01-05 16:06:23 +0100

    Updated game over behaviour

--- main.py
+++ main.py
@@ -24,21 +24,27 @@ def main():
     background = background.convert()
     background.fill((0, 0, 0))
 
-    font = pygame.font.Font(None, 64)
-    font2 = pygame.font.Font(None, 32)
-    game_over_text = font.render("Game over", True, (255, 255, 255))
-    over_text_pos = game_over_text.get_rect(centerx=background.get_width() / 2, y = background.get_height() / 2)
-
     clock = pygame.time.Clock()
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     main_player = Player(x, y)
     timer = TIMED_MISSION
+    game_over = False
 
 
     dt = 0
+
+    font = pygame.font.Font(None, 64)
+    font2 = pygame.font.Font(None, 32)
+    game_over_text = font.render("Game over", True, (255, 255, 255))
+    over_text_pos = game_over_text.get_rect(centerx=background.get_width() / 2, y = background.get_height() / 2)
+
+
+
     while True:
-        if timer > 0:
+        if game_over == False:
+            if int(timer) == 210:
+                game_over = True
 
             log_state()
             screen.blit(background, (0, 0))
@@ -50,22 +56,29 @@ def main():
                 if asteroid.collides_with(main_player):
                     log_event("player_hit")
                     print("Game over!")
-                    sys.exit()
+                    game_over = True
+                    break
             for asteroid in asteroids:
                 for shot in shots:
                     if asteroid.collides_with(shot):
                         log_event("asteroid_shot")
                         shot.kill()
                         asteroid.split()
+                        main_player.score += 1
+            timer -= clock.get_time() / 1000
+            dt = clock.tick(60) / 1000
+            timer_text = font2.render(f"{int(timer)}", True,  (10, 10, 10))
+            timer_text_pos = timer_text.get_rect(centerx=background.get_width() / 2, y = 10)
+            screen.blit(timer_text, timer_text_pos)
         else:
+            score_text = font2.render(f"{main_player.score}", True, (255, 255, 255))
+            score_text_pos = score_text.get_rect(centerx=background.get_width() / 2, y = (background.get_height() / 2) + 64 )
             screen.blit(background, (0, 0))
             background.blit(game_over_text, over_text_pos)
-            pygame.quit()
-        timer -= clock.get_time() / 1000
-        dt = clock.tick(60) / 1000
-        timer_text = font2.render(f"{int(timer)}", True,  (10, 10, 10))
-        timer_text_pos = timer_text.get_rect(centerx=background.get_width() / 2, y = 10)
-        screen.blit(timer_text, timer_text_pos)
+            background.blit(score_text, score_text_pos)
+            game_over = True
+            
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
